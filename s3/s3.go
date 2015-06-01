6708416d325223005392401568cf55292f3da464
Mark Cox <mcox@localhost.localdomain>
2015-06-01 17:29:17 +1200

    Changed time zone in date headers from UTC to GMT, to work with Cloudian and match Amazon S3 examples

--- s3/s3.go
+++ s3/s3.go
@@ -980,7 +980,14 @@ func (s3 *S3) prepare(req *request) error {
 	}
 	reqSignpathSpaceFix := (&url.URL{Path: signpath}).String()
 	req.headers["Host"] = []string{u.Host}
-	req.headers["Date"] = []string{time.Now().In(time.UTC).Format(time.RFC1123)}
+
+	// Use GMT instead of UTC as the time zone.
+	// All the examples in the S3 documentation use GMT.
+	// Some S3-compatiblestorage providers (e.g Cloudian) require GMT
+	// rather than UTC (as of June 2015)
+	gmtDate := time.Now().In(time.FixedZone("GMT", 0)).Format(time.RFC1123)
+	req.headers["Date"] = []string{gmtDate}
+
 	if s3.Auth.Token() != "" {
 		req.headers["X-Amz-Security-Token"] = []string{s3.Auth.Token()}
 	}
