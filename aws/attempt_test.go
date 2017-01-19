06d935f310f32e0cdf39e5020df20c743078bb49
Tyler Power <tylerpowerx@gmail.com>
2017-01-19 15:15:49 +1300

    Attempt and AttemptStrategy are now interfaces so that different algorithms can be supplied by 3rd parties

--- aws/attempt_test.go
+++ aws/attempt_test.go
@@ -8,14 +8,14 @@ import (
 )
 
 func (S) TestAttemptTiming(c *C) {
-	testAttempt := aws.AttemptStrategy{
+	testAttempt := aws.FixedAttemptStrategy{
 		Total: 0.25e9,
 		Delay: 0.1e9,
 	}
 	want := []time.Duration{0, 0.1e9, 0.2e9, 0.2e9}
 	got := make([]time.Duration, 0, len(want)) // avoid allocation when testing timing
 	t0 := time.Now()
-	for a := testAttempt.Start(); a.Next(); {
+	for a := testAttempt.Start(); a.Next(nil); {
 		got = append(got, time.Now().Sub(t0))
 	}
 	got = append(got, time.Now().Sub(t0))
@@ -31,28 +31,28 @@ func (S) TestAttemptTiming(c *C) {
 }
 
 func (S) TestAttemptNextHasNext(c *C) {
-	a := aws.AttemptStrategy{}.Start()
-	c.Assert(a.Next(), Equals, true)
-	c.Assert(a.Next(), Equals, false)
+	a := aws.FixedAttemptStrategy{}.Start()
+	c.Assert(a.Next(nil), Equals, true)
+	c.Assert(a.Next(nil), Equals, false)
 
-	a = aws.AttemptStrategy{}.Start()
-	c.Assert(a.Next(), Equals, true)
+	a = aws.FixedAttemptStrategy{}.Start()
+	c.Assert(a.Next(nil), Equals, true)
 	c.Assert(a.HasNext(), Equals, false)
-	c.Assert(a.Next(), Equals, false)
+	c.Assert(a.Next(nil), Equals, false)
 
-	a = aws.AttemptStrategy{Total: 2e8}.Start()
-	c.Assert(a.Next(), Equals, true)
+	a = aws.FixedAttemptStrategy{Total: 2e8}.Start()
+	c.Assert(a.Next(nil), Equals, true)
 	c.Assert(a.HasNext(), Equals, true)
 	time.Sleep(2e8)
 	c.Assert(a.HasNext(), Equals, true)
-	c.Assert(a.Next(), Equals, true)
-	c.Assert(a.Next(), Equals, false)
+	c.Assert(a.Next(nil), Equals, true)
+	c.Assert(a.Next(nil), Equals, false)
 
-	a = aws.AttemptStrategy{Total: 1e8, Min: 2}.Start()
+	a = aws.FixedAttemptStrategy{Total: 1e8, Min: 2}.Start()
 	time.Sleep(1e8)
-	c.Assert(a.Next(), Equals, true)
+	c.Assert(a.Next(nil), Equals, true)
 	c.Assert(a.HasNext(), Equals, true)
-	c.Assert(a.Next(), Equals, true)
+	c.Assert(a.Next(nil), Equals, true)
 	c.Assert(a.HasNext(), Equals, false)
-	c.Assert(a.Next(), Equals, false)
+	c.Assert(a.Next(nil), Equals, false)
 }
